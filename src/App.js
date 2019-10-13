3e8f7a8e4b497ecc01a9812c1d0ba89762606a1a
Rafael Moreno Higueras <rmh1516@gmail.com>
2019-10-13 18:57:01 +0200

    loading spinner

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Suspense } from "react";
 import "./assets/css/normalize.css";
 import "./assets/css/skeleton.css";
 import img from "./assets/images/cryptomonedas.png";
 import Form from "./components/Form";
+import Spinner from "./components/Spinner";
 
 function App() {
   const [cryptoCurrency, setCryptoCurrency] = useState("");
   const [currency, setCurrency] = useState("");
+  const [data, setData] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    if (currency === "") return;
+    setIsLoading(true);
     const quoteCryptocurrency = async () => {
       const url = `https://min-api.cryptocompare.com/data/price?fsym=${cryptoCurrency}&tsyms=${currency}`;
       const result = await fetch(url);
       const data = await result.json();
-      console.log(data);
+      console.log(data[currency]);
     };
     quoteCryptocurrency();
   }, [cryptoCurrency, currency]);
 
+  const component = isLoading && <Spinner />;
   return (
     <div className="container">
       <div className="row">
@@ -25,11 +31,12 @@ function App() {
           <img src={img} alt="crypto" className="logotipo" />
         </div>
         <div className="one-half column">
-          <h1>Instant Cryptocurrency quotes</h1>
+          <h1>Cryptocurrencies</h1>
           <Form
             setCryptoCurrency={setCryptoCurrency}
             setCurrency={setCurrency}
           />
+          {component}
         </div>
       </div>
     </div>
